dea11e2f6bd12a8d7f53b8cf4f497986e66a762d
Linoy Malol <linoymalol1@gmail.com>
2022-01-18 14:43:24 +0200

    Added Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
-import MadaReports.ListOfMadaReports;
-import MadaReports.MadaReports;
+import etl.EtlManager;
+import etl.extarct.Extract;
+import etl.extarct.csv.CsvExtract;
+import madaReports.ListOfMadaReports;
+import madaReports.MadaReports;
 import etl.extarct.csv.CsvParser;
 import etl.extarct.csv.CsvReader;
 import etl.load.json.JsonLoad;
@@ -12,21 +15,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       // Extract csv = new CsvReader();
         CsvReader csvReader = new CsvReader();
         CsvParser csvParser = new CsvParser();
         List<CSVRecord> extractCsv = csvParser.fileToList(csvReader.read("src/main/resources/MadaReports.csv"));
         ListOfMadaReports newListOfMadaReports= new ListOfMadaReports();
         List<MadaReports> newList = newListOfMadaReports.madaReportsList(extractCsv);
-        //  for (MadaReports m: newList) {
-        //    System.out.println(m.toString());
-      //  }
-        //int countOfFiles = 51000/50000 + 1;
-        //System.out.println(countOfFiles);
         JsonLoad jsonLoad = new JsonLoad();
-        //Object madaReports = new Object(); // jsonLoad.createFile( "C:\\mada_reports\\mada_reports_a.json");
-        jsonLoad.objectToJson("C:\\mada_reports\\mada_reports_a.json", newList);
-       // System.out.println(jsonLoad.newPath("C:\\mada_reports\\mada_reports_a.json", 2));
+        jsonLoad.load("C:\\mada_reports\\mada_reports_a.json", newList);
+        //EtlManager man = new EtlManager();
+       // man.extractTheFile("src/main/resources/MadaReports.csv");
+        //System.out.println(man.getExtensionByGuava("C:\\mada_reports\\mada_reports_a.csv"));
     }
 
 }
