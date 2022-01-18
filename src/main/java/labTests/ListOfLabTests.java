512240a6f2b772a9fdbda14d249f6916f7d0b3fd
Linoy Malol <linoymalol1@gmail.com>
2022-01-18 18:56:03 +0200

    Added ListOfLabTests class

--- src/main/java/labTests/ListOfLabTests.java
+++ src/main/java/labTests/ListOfLabTests.java
@@ -1,2 +1,19 @@
-package labTests;public class ListOfLabTests {
+package labTests;
+
+import org.apache.commons.csv.CSVRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfLabTests {
+    public List<LabTests> labTestsList(List<CSVRecord> listOfRecords) {
+        List<LabTests> labTestsList = new ArrayList<>();
+        boolean firstRecord = true;
+        for(CSVRecord record:listOfRecords){
+            if (!firstRecord){
+                labTestsList.add(new LabTests(record));}
+            firstRecord = false;
+        }
+        return labTestsList;
+    }
 }
