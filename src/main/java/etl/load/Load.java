a6f3f94d6bf4d84481c24234b74638134611e132
Linoy Malol <linoymalol1@gmail.com>
2022-01-18 14:41:45 +0200

    Added Load class

--- src/main/java/etl/load/Load.java
+++ src/main/java/etl/load/Load.java
@@ -1,9 +1,8 @@
 package etl.load;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 public interface Load <T> {
-    public void objectToJson(String path, List listOfObjects) throws IOException;
+    public void load(String path, List listOfObjects) throws IOException;
 }
