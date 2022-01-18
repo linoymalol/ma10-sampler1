93ab16a2f1137ee0da4f91c40589eb17723779dd
Linoy Malol <linoymalol1@gmail.com>
2022-01-18 14:40:03 +0200

    Added Reader interface

--- src/main/java/etl/extarct/Reader.java
+++ src/main/java/etl/extarct/Reader.java
@@ -1,8 +1,6 @@
 package etl.extarct;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
 public interface Reader <T> {
     Object read(String path) throws IOException;
